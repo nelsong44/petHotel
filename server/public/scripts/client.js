afa2c2cea8fcb8a0bba07d01df8f4dde27785a73
Amanda Kirchner <geeky.brewster@gmail.com>
2017-07-12 16:50:58 -0500

    Added comments describing needed AJAX requests

---
+++ server/public/scripts/client.js
@@ -0,0 +1,28 @@
+// Add click listeners for:
+
+// Add new customer button
+// AJAX: POST, url: "/pets/newCustomer"
+// data to send to server: first_name, last_name
+
+// Add new pet button
+// AJAX: POST, url: "/pets/newPet"
+// data to send to server: owner, name, breed, color
+
+// Update pet button
+// AJAX: PUT, url: "/pets" + id
+// data to send: pet id with the url
+
+// Delete pet button
+// AJAX: DELETE, url: "/pets" + id
+
+// Check in / check out button
+// AJAX to mark checked in: PUT, url: "/pets/checkIn" + id
+// data to send: pet id with the url
+
+// AJAX to mark checked out: PUT, url: "/pets/checkOut" + id
+// data to send: pet id with the url
+
+
+// function to displayPets
+// AJAX: GET
+// data to receive: owner, name, breed, color
