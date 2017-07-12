bbaa945632dcf2b7b259a8f24a92a967e7ab96c5
Amanda Kirchner <geeky.brewster@gmail.com>
2017-07-12 16:04:20 -0500

    Server.js file updated. Basic server started and serves static files.

---
+++ server/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var path = require('path');
+var port = process.env.PORT || 7000;
+var petsRoute = require('./routes/pets.js');
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/pets', petsRoute);
+
+
+// Serve back static files by default
+app.get('/*', function(req, res){
+  var file = req.params[0] || '/views/index.html';
+  res.sendFile(path.join(__dirname, '/public/', file));
+});
+
+// Start listenting for requests at given PORT
+app.listen(port, function(){
+  console.log('All ears on port: ', port);
+});
