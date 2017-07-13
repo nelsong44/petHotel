efea3209f8a4ffe8517b6b79626497b61209a6dd
nelsong44 <nelsong44@gmail.com>
2017-07-12 21:45:38 -0500

    pool module created and sourced, static files sourced

---
+++ server/routes/pets.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var router = express.Router();
+var poolModule = require('../modules/pool.js');
+var pool = poolModule;
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,12 +3,11 @@ var app = express();
 var bodyParser = require('body-parser');
 var path = require('path');
 var port = process.env.PORT || 7000;
-var petsRoute = require('./routes/pets.js');
+var petsRoute = require('./routes/pets');
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use('/pets', petsRoute);
-
 
 // Serve back static files by default
 app.get('/*', function(req, res){
